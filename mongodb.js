83c8677d744e8bf32384019c30837114a1663b24
Norman Robert <normanrobertf@gmail.com>
2022-04-16 00:28:35 +0800

    explored mongodb insertone and insertmany

--- mongodb.js
+++ mongodb.js
@@ -13,8 +13,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'fathernorms',
-        age: 29
+    // db.collection('users').insertOne({
+    //     name: 'fathernorms',
+    //     age: 29
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user into database.')
+    //     }
+
+    //     console.log(result.insertedId)
+    // })
+
+    //  db.collection('users').insertMany([
+    //      {
+    //          name: 'user1',
+    //          age: 22
+    //      }, {
+    //          name: 'user2',
+    //          age: 23
+    //      }
+    //  ], (error, result) => {
+    //      if (error) {
+    //          return console.log('Unable to insert user into database')
+    //      }
+    //      console.log(result.insertedIds)
+    //  })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'First Task',
+            completed: true,
+        }, {
+            description: 'Second Task',
+            completed: false,
+        }, {
+            description: 'Work on Task App',
+            completed: false,
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks into tasks collection.')
+        }
+        console.log(result.acknowledged)
     })
 })
