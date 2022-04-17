26f01acd25f58e212bf33eaebb57a87728c91ab4
Norman Robert <normanrobertf@gmail.com>
2022-04-17 23:41:14 +0800

    added hashing for plaintext password, login

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const User = require("../models/user");
 const router = new express.Router();
@@ -11,6 +10,18 @@ router.post("/users", async (req, res) => {
         res.status(201).send(user);
     } catch (error) {
         res.status(400).send(error);
+    }
+});
+
+router.post("/users/login", async (req, res) => {
+    try {
+        const user = await User.findByCredentials(
+            req.body.email,
+            req.body.password
+        );
+        res.send(user);
+    } catch (error) {
+        res.status(400).send();
     }
 });
 
@@ -47,13 +58,15 @@ router.patch("/users/:id", async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
+        const user = await User.findById(req.params.id);
+
         if (!user) {
             return res.status(404).send();
         }
+        updates.forEach((update) => {
+            user[update] = req.body[update];
+        });
+        await user.save();
         res.send(user);
     } catch (error) {
         res.status(400).send(error);
